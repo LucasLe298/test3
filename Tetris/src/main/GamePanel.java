0e057948488f557111e0ec9c51f25158946939f2
Huy <vohuy060214@gmail.com>
2023-09-28 23:03:14 +0700

    first commit

---
+++ Tetris/src/main/GamePanel.java
@@ -0,0 +1,72 @@
+package main;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements Runnable{
+	
+	public static final int WIDTH = 1000;
+	public static final int HEIGHT = 720;
+	public int FPS = 60;
+	Thread gameThread;
+	PlayManager pm;
+	
+	public GamePanel() {
+		
+		// panel settings
+		this.setPreferredSize(new Dimension(WIDTH,HEIGHT));
+		this.setBackground(Color.black);
+		this.setLayout(null);
+		
+		//implement keylist
+		this.addKeyListener(new KeyHandler());
+		this.setFocusable(true);
+		
+		pm = new PlayManager();
+	}
+	public void launchGame() {
+		
+		gameThread = new Thread(this);
+		gameThread.start();
+	}
+
+	@Override
+	public void run() {
+		// game loop
+		double drawInterval = 1000000000/FPS;
+		double delta = 0;
+		long lastTime = System.nanoTime();
+		long currentTime;
+		
+		while(gameThread != null) {
+			currentTime = System.nanoTime();
+			
+			delta += (currentTime - lastTime) / drawInterval;
+			lastTime = currentTime;
+			
+			if(delta >= 1) {
+				update();
+				repaint();
+				delta--;
+			}
+		}
+	}
+	
+	private void update() {
+		if(KeyHandler.pause == false) {
+			pm.update();
+		}
+	}
+	
+	public void paintComponent(Graphics g)  {
+		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D)g;
+		pm.draw(g2);
+	}
+
+	
+}
